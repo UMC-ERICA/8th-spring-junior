b6d3d2767620aad06b8a1cd22cc56ea9edc98a85
nawkwoo <sonkw4775@naver.com>
2025-05-02 16:33:00 +0900

    [feat] re commit

--- 파누/spring/src/main/java/umc/study/Application.java
+++ 파누/spring/src/main/java/umc/study/Application.java
@@ -1,7 +1,9 @@
-package com.example.spring;
+//package com.example.spring;
+package umc.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +12,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 
 }
---
+++ 파누/spring/src/main/java/umc/study/domain/Restaurant.java
@@ -0,0 +1,35 @@
+package umc.study.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+import lombok.Getter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
+
+import umc.study.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Restaurant extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false)
+    private String location;
+
+    private Double score;
+}
