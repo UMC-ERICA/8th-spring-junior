442276f490e24d99522c2c50206d0faa6287bd4d
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2025-05-07 01:36:45 +0900

    feat: Store 조회 서비스 인터페이스 및 구현 추가

---
+++ 라미/spring/src/main/java/umc/study/service/StoreService/StoreQueryService.java
@@ -0,0 +1,14 @@
+package umc.study.service.StoreService;
+
+import umc.study.domain.Store;
+import umc.study.domain.enums.StoreStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StoreQueryService {
+
+    Optional<Store> findStore(Long id);
+
+    List<Store> findStoresByNameAndStatus(String name, StoreStatus status);
+}
