837adc8a2ab4b3a94cc773c33a960a6a86b20919
nawkwoo <sonkw4775@naver.com>
2025-05-07 02:25:59 +0900

    작업 중 변경사항 커밋

--- 파누/spring/src/main/java/umc/study/config/QueryDSLConfig.java
+++ 파누/spring/src/main/java/umc/study/config/QueryDSLConfig.java
@@ -1,5 +1,7 @@
 package umc.study.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class QueryDSLConfig {
+
     private final EntityManager entityManager;
 
     @Bean
