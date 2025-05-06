63347304ee3389aa16a8c45de8ee5831ada0287d
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2025-05-07 01:35:59 +0900

    feat: Store 레포지토리 커스텀 구현

---
+++ 라미/spring/src/main/java/umc/study/repository/StoreRepository/StoreRepositoryCustom.java
@@ -0,0 +1,10 @@
+package umc.study.repository.StoreRepository;
+
+import umc.study.domain.Store;
+import umc.study.domain.enums.StoreStatus;
+
+import java.util.List;
+
+public interface StoreRepositoryCustom {
+    List<Store> dynamicQueryWithBooleanBuilder(String name, StoreStatus status);
+}
