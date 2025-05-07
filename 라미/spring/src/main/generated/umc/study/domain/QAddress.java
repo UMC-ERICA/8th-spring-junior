bd976cd6818e0e1e774059a510cb5abe79c4ba4f
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2025-05-07 17:41:52 +0900

    feat: QueryDSL Q 클래스 생성 파일 추가

---
+++ 라미/spring/src/main/generated/umc/study/domain/QAddress.java
@@ -0,0 +1,69 @@
+package umc.study.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QAddress is a Querydsl query type for Address
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QAddress extends EntityPathBase<Address> {
+
+    private static final long serialVersionUID = 1876986034L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QAddress address = new QAddress("address");
+
+    public final umc.study.domain.common.QBaseEntity _super = new umc.study.domain.common.QBaseEntity(this);
+
+    public final StringPath city = createString("city");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final StringPath detail = createString("detail");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath postalCode = createString("postalCode");
+
+    public final StringPath state = createString("state");
+
+    public final QStore store;
+
+    public final StringPath street = createString("street");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QAddress(String variable) {
+        this(Address.class, forVariable(variable), INITS);
+    }
+
+    public QAddress(Path<? extends Address> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QAddress(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QAddress(PathMetadata metadata, PathInits inits) {
+        this(Address.class, metadata, inits);
+    }
+
+    public QAddress(Class<? extends Address> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.store = inits.isInitialized("store") ? new QStore(forProperty("store"), inits.get("store")) : null;
+    }
+
+}
+
