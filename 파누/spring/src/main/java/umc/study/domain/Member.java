1a01189d9048da3b6c185d3f145613d4bc654f76
nawkwoo <sonkw4775@naver.com>
2025-05-23 14:48:00 +0900

    [feat] 8주차 미션

--- 파누/spring/src/main/java/umc/study/domain/Member.java
+++ 파누/spring/src/main/java/umc/study/domain/Member.java
@@ -1,36 +1,20 @@
 package umc.study.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.CascadeType;
-
-import lombok.Getter;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import lombok.AccessLevel;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.ArrayList;
-
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import umc.study.domain.common.BaseEntity;
 import umc.study.domain.enums.Gender;
 import umc.study.domain.enums.SocialType;
 import umc.study.domain.enums.MemberStatus;
-
 import umc.study.domain.mapping.MemberAgree;
 import umc.study.domain.mapping.MemberPrefer;
 import umc.study.domain.mapping.MemberMission;
-import umc.study.domain.Review;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -56,7 +40,7 @@ public class Member extends BaseEntity {
 
     private LocalDate inactiveDate;
 
-//    @Column(nullable = false)
+    // @Column(nullable = false)
     private String email;
 
     private Integer point;
@@ -71,14 +55,18 @@ public class Member extends BaseEntity {
     private MemberStatus status;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    @Builder.Default
     private List<MemberAgree> memberAgreeList = new ArrayList<>();
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    @Builder.Default
     private List<MemberPrefer> memberPreferList = new ArrayList<>();
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    @Builder.Default
     private List<Review> reviewList = new ArrayList<>();
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    @Builder.Default
     private List<MemberMission> memberMissionList = new ArrayList<>();
 }
